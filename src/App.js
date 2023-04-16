350204ce1f67278beab2dd692bb26a68257dc4d3
Divine Orji <dpkreativ@gmail.com>
2023-04-16 14:14:59 +0100

    chore: set up file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./App.css";
-import { NavBar } from "./components/NavBar";
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      {/* Header Section */}
+      <Header />
+
+      {/* Main Section */}
+      <MainContent />
+
+      {/* Footer Section */}
+      <Footer />
     </div>
   );
 }
